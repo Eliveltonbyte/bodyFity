2c8847d6bc938de92f3341bd195dc70b16a302fc
Eliveltonbyte <76592362+Eliveltonbyte@users.noreply.github.com>
2023-02-07 21:00:04 -0300

    Add files via upload

---
+++ Frames/Frame.FichaExercicio.pas
@@ -0,0 +1,28 @@
+unit Frame.FichaExercicio;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Objects, FMX.Controls.Presentation;
+
+type
+  TFrameFichaExercicio = class(TFrame)
+    Rectangle5: TRectangle;
+    lblTitulo: TLabel;
+    Image3: TImage;
+    lblSubtitulo: TLabel;
+    Rectangle1: TRectangle;
+    CheckBox1: TCheckBox;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.fmx}
+
+end.
---
+++ Frames/Frame.Treino.pas
@@ -0,0 +1,26 @@
+unit Frame.Treino;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Objects, FMX.Controls.Presentation;
+
+type
+  TFrameTreino = class(TFrame)
+    Rectangle5: TRectangle;
+    lblTitulo: TLabel;
+    Image3: TImage;
+    lblSubtitulo: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.fmx}
+
+end.
